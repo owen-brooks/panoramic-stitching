65041237b5bbfe2d2dbb99133114cb6ccfa73473
Taha <tahagabre@gmail.com>
2020-03-14 20:00:32 -0400

    pt2 WIP

--- src/run.m
+++ src/run.m
@@ -7,7 +7,7 @@ pic1 = imread('./pics/pic1.jpeg'); pic2 = imread('./pics/pic2.jpeg');
 pic1_pts = [375 750;1210 425;780 470;555 120;];
 pic2_pts = [35 775; 890 450; 505 470;280 85;];
 
-figure(1); hold on; imshow([pic1 pic2]);
+%figure(1); hold on; imshow([pic1 pic2]);
 for i = 1:4
     hold on;
     plot([pic1_pts(i, 1) w1+ pic2_pts(i, 1)], [pic1_pts(i, 2) pic2_pts(i, 2)], '.', 'MarkerSize', 30);
@@ -15,10 +15,9 @@ end
 hold off;
 
 %%%%%% 2 %%%%%%
-figure(2);
+%figure(2);
 blended_im = transform_and_blend(pic1, pic2, pic1_pts, pic2_pts);
-imshow(blended_im)
-
+imshow(blended_im);
 
 %%%%%% 3 %%%%%%
 % figure(3), hold on
--- src/transform_and_blend.m
+++ src/transform_and_blend.m
@@ -1,5 +1,8 @@
 function targetImage = transform_and_blend(pic1, pic2, pic1_pts, pic2_pts)
-    % Projected Guess
+    % pic1 is defined as base image
+    % pic2 is image that will be projected onto pic1
+    
+% Projected Value
     xHat_1 = pic2_pts(1, 1);
     yHat_1 = pic2_pts(1, 2);
 
@@ -11,8 +14,8 @@ function targetImage = transform_and_blend(pic1, pic2, pic1_pts, pic2_pts)
 
     xHat_4 = pic2_pts(4, 1);
     yHat_4 = pic2_pts(4, 2);
-
-    % Observed Value
+ 
+% Base Value
     x_1 = pic1_pts(1, 1);
     y_1 = pic1_pts(1, 2);
 
@@ -25,60 +28,62 @@ function targetImage = transform_and_blend(pic1, pic2, pic1_pts, pic2_pts)
     x_4 = pic1_pts(4, 1);
     y_4 = pic1_pts(4, 2);
 
-    A = [-x_1, -y_1, -1, 0, 0, 0, xHat_1 * x_1, xHat_1 * y_1, xHat_1;
-        0, 0, 0, -x_1, -y_1, -1, yHat_1 * x_1, yHat_1 * y_1, yHat_1;
-        -x_2, -y_2, -1, 0, 0, 0, xHat_2 * x_2, xHat_2 * y_2, xHat_2;
-        0, 0, 0, -x_2, -y_2, -1, yHat_2 * x_2, yHat_2 * y_2, yHat_2;
-        -x_3, -y_3, -1, 0, 0, 0, xHat_3 * x_3, xHat_3 * y_3, xHat_3;
-        0, 0, 0, -x_3, -y_3, -1, yHat_3 * x_3, yHat_3 * y_3, yHat_3;
-        -x_4, -y_4, -1, 0, 0, 0, xHat_4 * x_4, xHat_4 * y_4, xHat_4;
-        0, 0, 0, -x_4, -y_4, -1, yHat_4 * x_4, yHat_4 * y_4, yHat_4;];
+    A = [-x_1, -y_1, -1, 0, 0, 0, xHat_1 .* x_1, xHat_1 .* y_1, xHat_1;
+        0, 0, 0, -x_1, -y_1, -1, yHat_1 .* x_1, yHat_1 .* y_1, yHat_1;
+        
+        -x_2, -y_2, -1, 0, 0, 0, xHat_2 .* x_2, xHat_2 .* y_2, xHat_2;
+        0, 0, 0, -x_2, -y_2, -1, yHat_2 .* x_2, yHat_2 .* y_2, yHat_2;
+        
+        -x_3, -y_3, -1, 0, 0, 0, xHat_3 .* x_3, xHat_3 .* y_3, xHat_3;
+        0, 0, 0, -x_3, -y_3, -1, yHat_3 .* x_3, yHat_3 .* y_3, yHat_3;
+        
+        -x_4, -y_4, -1, 0, 0, 0, xHat_4 .* x_4, xHat_4 .* y_4, xHat_4;
+        0, 0, 0, -x_4, -y_4, -1, yHat_4 .* x_4, yHat_4 .* y_4, yHat_4;];
 
-    [left, sigma, right] = svd(transpose(A) * A);
-    diag = [];
-
-    % find SMALLEST value in diagonal of s
-    for i = 1:size(sigma)
-        diag(i) = sigma(i, i);
-    end
-
-    [smallest, i] = min(diag);
-    % Whatever column that value is in, take the column of l, and that is our h
-    % values
-    h = left(:, i);
+    [~, ~, eigenV] = svd(transpose(A)*A);
+    h = eigenV(:, end);
     % then reshape it to a 3x3, and you have your transformation matrix H
+    % H = normalize(reshape(h, [3,3]));
     H = reshape(h, [3,3]);
+    
+    pic2_upperLeft = [0, 0, 1];
+    pic2_bottomLeft = [0, size(pic2, 1), 1];
+    pic2_upperRight = [size(pic2, 2), 0, 1];
+    pic2_bottomRight = [size(pic2, 2), size(pic2, 1), 1];
 
-    upperLeft = [0, 0, 1];
-    bottomLeft = [0, size(pic2, 1), 1];
-    upperRight = [size(pic2, 2), 0, 1];
-    bottomRight = [size(pic2, 2), size(pic2, 1), 1];
-
-    projectingUL = upperLeft * H;
-    projectingBL = bottomLeft * H;
-    projectingUR = upperRight * H;
-    projectingBR = bottomRight * H;
-
+    projectingUL = pic2_upperLeft * inv(H);
+    projectingUL = projectingUL / projectingUL(3);
+    
+    projectingBL = pic2_bottomLeft * inv(H);
+    projectingBL = projectingBL / projectingBL(3);
+    
+    projectingUR = pic2_upperRight * inv(H);
+    projectingUR = projectingUR / projectingUR(3);
+    
+    projectingBR = pic2_bottomRight * inv(H);
+    projectingBR = projectingBR / projectingBR(3);
+    
+    projectingXY = [projectingUL(1) projectingUL(2);
+        projectingBL(1) projectingBL(2);
+        projectingUR(1) projectingUR(2);
+        projectingBR(1) projectingBR(2);];
+    
+    marked = insertMarker(pic1, projectingXY, 'o', 'color', 'white', 'size', 50);
+    imshow(marked);
+    
     % The height of this image should be the maximum of the base image's height or the
     % maximum projected y value from the other image. The width will be equal to the maximum of the
     % base image's width or the maximum projected x value from the other image.
 
     % Decide dimensions of target image
-    baseImageHeight = size(pic1, 1);
-    projectedImageHeight = max(baseImageHeight - projectingBL(2), baseImageHeight - projectingBR(2));
-    targetImageHeight = max(baseImageHeight, projectedImageHeight);
-
-    baseImageWidth = size(pic1, 2);
-    projectedImageWidth = max(baseImageWidth - projectingUR(1), baseImageWidth - projectingBR(1));
-    targetImageWidth = max(baseImageWidth, projectedImageWidth);
-
-    projectedImageHeight = round(projectedImageHeight);
-    projectedImageWidth = round(projectedImageWidth);
-
-    % Distance offsets of projection on base 
-    % DX = targetImage_origin_x - baseImage_origin_x
+    
+    projectingMaxX = max([projectingUL(1), projectingUR(1), projectingBL(1), projectingBR(1)]);
+    projectingMaxY = max([projectingUL(2), projectingUR(2), projectingBL(2), projectingBR(2)]);
+    targetImageWidth = max(size(pic1, 2), projectingMaxX);
+    targetImageHeight = max(size(pic1, 1), projectingMaxY);
+    
+    % Distance from projection origin to base origin in target image coordinates 
     DX = projectingUL(1);
-    % DY = targetImage_origin_y - baseImage_origin_y
     DY = projectingUL(2);
 
     % Matrix to move from target image to base image
@@ -91,19 +96,22 @@ function targetImage = transform_and_blend(pic1, pic2, pic1_pts, pic2_pts)
         for width = 1 : round(targetImageWidth)
             for height = 1 : round(targetImageHeight)
                 % use translationMatrix to go to base image, return (bX, bY, channel)
-                basePixelLocation = translationMatrix * [width; height; 1]
-                H
+                basePixelLocation = translationMatrix * [-width; -height; 1];
+                
                 % use H to go from base image to projected, return (pX, pY, channel)
-                projectedPixelLocation = H * [basePixelLocation(1); basePixelLocation(2); 1]
+                projectedPixelLocation = H * [basePixelLocation(1); basePixelLocation(2); 1];
+                projectedPixelLocation = projectedPixelLocation / projectedPixelLocation(3,1);
 
-                %if bX AND bY is positive, record the value at base(bX, bY, channel)
-                if basePixelLocation(1) > 0 && basePixelLocation(2) > 0
+                % if bX AND bY is positive, record the value at base(bX, bY, channel)
+                if (basePixelLocation(1,1) > 0 && basePixelLocation(2,1) > 0)
                     targetImage(height, width, channel) = 1;
+                    % continue;
                 end
-
-                % if pX AND pY is positive, record the value at projected(pX, pY, channel)
-                if projectedPixelLocation(1) > 0 && projectedPixelLocation(2) > 0
-                    targetImage(height, width, channel) = 2;
+% 
+%                 % if pX AND pY is positive, record the value at projected(pX, pY, channel)
+                if (projectedPixelLocation(1,1) > 0 && projectedPixelLocation(2,1) > 0)
+                    targetImage(height, width, channel) = 255;
+                    % continue;
                 end
 
                 % if both are positive, blend them
