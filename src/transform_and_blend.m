fd27da0c7d57a9d6b6b60dfbfe481117a45541ec
Owen Brooks <oqb23@drexel.edu>
2020-03-15 17:44:49 -0400

    add part 2 code WIP

--- src/transform_and_blend.m
+++ src/transform_and_blend.m
@@ -109,6 +109,10 @@ function targetImage = transform_and_blend(pic1, pic2, pic1_pts, pic2_pts)
                     targetImage(height, width, :) = pic1(pp(2), pp(1), :);
                  
             end
+            
+            if (w1 >= bp(1) && bp(1) > 0) && (h1 >= bp(2) && bp(2) > 0)
+                targetImage(height, width, :) = pic2(bp(2), bp(1), :);
+            end
                 
             
             % if pX AND pY is positive, record the value at projected(pX, pY, channel)
