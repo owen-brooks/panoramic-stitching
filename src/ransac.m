82bd8e21d54c002327a69f87e93b90e54643e94f
Owen Brooks <oqb23@drexel.edu>
2020-03-17 11:27:41 -0400

    fix ransac to use all kps

--- src/ransac.m
+++ src/ransac.m
@@ -4,12 +4,13 @@ function kp_used = ransac(kps1, kps2, N)
         % select 4 rand keypoint matches
         temp_idxs = randperm(length(kps1), 4);idxs(i,:) = temp_idxs;
         H = transformation_mat(kps1(temp_idxs,:), kps2(temp_idxs,:));
-        pic2_upperLeft = [kps2(temp_idxs,:)' ; ones(1, 4)];projectingUL = H * pic2_upperLeft;
+        pic2_upperLeft = [kps2' ; ones(1, size(kps2,1))];
+        projectingUL = H * pic2_upperLeft;
         
         score = 0; % score is sum of euclidean dists
-        for ii=1:4
-            temp_kps2 = [projectingUL(1) projectingUL(2)]./projectingUL(3);
-            temp_kps1 = kps1(temp_idxs,:);temp_kps1 = temp_kps1(ii,:);
+        for ii=1:size(kps2,1)
+            temp_kps2 = [projectingUL(1,ii) projectingUL(2,ii)]./projectingUL(3,ii);
+            temp_kps1 = [kps1(ii,1) kps1(ii,2)];
             score  = score + sqrt(sum(temp_kps1-temp_kps2).^2);
         end
         scores(i) = score;
